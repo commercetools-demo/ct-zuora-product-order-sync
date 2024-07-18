830aadbd371652e2ddc151837bfc4face948e096
Behnam Tehrani <behnam777@gmail.com>
2024-07-18 13:40:15 -0400

    Revert "Retry ct calles"
    This reverts commit 8ad1c5f0bedde625f4f508e5d26bb6efbc210d83.

--- event/src/controllers/ct.controller.ts
+++ event/src/controllers/ct.controller.ts
@@ -6,45 +6,19 @@ import {
 import { createApiRoot } from '../client/create.client';
 import { logger } from '../utils/logger.utils';
 
-async function retryPromise<T>(
-  promiseFactory: () => Promise<T>,
-  maxAttempts = 5,
-  delayMs = 1000
-): Promise<T> {
-  let lastError: Error | null = null;
-
-  for (let attempt = 1; attempt <= maxAttempts; attempt++) {
-    try {
-      return await promiseFactory();
-    } catch (error) {
-      lastError = error as Error;
-      logger.info(`Attempt ${attempt} failed. Retrying...`);
-
-      if (attempt < maxAttempts) {
-        await new Promise((resolve) => setTimeout(resolve, delayMs));
-      }
-    }
-  }
-
-  throw new Error(
-    `All ${maxAttempts} attempts failed. Last error: ${lastError?.message}`
-  );
-}
-
 export const getProductById = async (
   id: string
 ): Promise<ProductProjection | null> => {
   try {
-    const product = await retryPromise(() =>
-      createApiRoot()
-        .productProjections()
-        .get({
-          queryArgs: {
-            where: `id="${id}"`,
-          },
-        })
-        .execute()
-    ).then((result) => result.body.results?.[0]);
+    const product = await createApiRoot()
+      .productProjections()
+      .get({
+        queryArgs: {
+          where: `id="${id}"`,
+        },
+      })
+      .execute()
+      .then((result) => result.body.results?.[0]);
 
     return product;
   } catch (error) {
@@ -54,9 +28,12 @@ export const getProductById = async (
 };
 export const getOrderById = async (id: string): Promise<Order | null> => {
   try {
-    const order = await retryPromise(() =>
-      createApiRoot().orders().withId({ ID: id }).get().execute()
-    ).then((result) => result.body);
+    const order = await createApiRoot()
+      .orders()
+      .withId({ ID: id })
+      .get()
+      .execute()
+      .then((result) => result.body);
 
     return order;
   } catch (error) {
@@ -67,9 +44,12 @@ export const getOrderById = async (id: string): Promise<Order | null> => {
 };
 export const getCustomerById = async (id: string): Promise<Customer | null> => {
   try {
-    const customer = await retryPromise(() =>
-      createApiRoot().customers().withId({ ID: id }).get().execute()
-    ).then((result) => result.body);
+    const customer = await createApiRoot()
+      .customers()
+      .withId({ ID: id })
+      .get()
+      .execute()
+      .then((result) => result.body);
 
     return customer;
   } catch (error) {
